1266025d4f944baf19621e0e75246513d1c07aca
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2023-12-12 21:56:22 -0300

    feat: exportando componente products e consumindo em host-app

--- projects/host-app/webpack.config.js
+++ projects/host-app/webpack.config.js
@@ -1,58 +1,18 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, '../../tsconfig.json'),
-  [/* mapped paths to share */]);
-
-module.exports = {
-  output: {
-    uniqueName: "hostApp",
-    publicPath: "auto"
+module.exports = withModuleFederationPlugin({
+  // Aqui estou consumindo o componente products que eu dei um exposes no webpack.config.js dele.
+  remotes: {
+    products: "http://localhost:3000/remoteEntry.js",
   },
-  optimization: {
-    runtimeChunk: false
-  },   
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
-  },
-  experiments: {
-    outputModule: true
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-        library: { type: "module" },
-
-        // For remotes (please adjust)
-        // name: "hostApp",
-        // filename: "remoteEntry.js",
-        // exposes: {
-        //     './Component': './projects/host-app/src/app/app.component.ts',
-        // },        
-        
-        // For hosts (please adjust)
-        // remotes: {
-        //     "productsApp": "http://localhost:4200/remoteEntry.js",
-        //     "cartsApp": "http://localhost:4200/remoteEntry.js",
-        //     "ordersApp": "http://localhost:4200/remoteEntry.js",
-
-        // },
-
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
-          ...sharedMappings.getDescriptors()
-        })
-        
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
     }),
-    sharedMappings.getPlugin()
-  ],
-};
+  },
+});
---
+++ projects/products-app/webpack.config copy.js
@@ -0,0 +1,73 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, "../../tsconfig.json"), [
+  /* mapped paths to share */
+]);
+
+module.exports = {
+  output: {
+    uniqueName: "productsApp",
+    publicPath: "auto",
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      library: { type: "module" },
+
+      // For remotes (please adjust)
+      // name: "productsApp",
+      // filename: "remoteEntry.js",
+      // exposes: {
+      //     './Component': './projects/products-app/src/app/app.component.ts',
+      // },
+
+      // For hosts (please adjust)
+      // remotes: {
+      //     "hostApp": "http://localhost:5000/remoteEntry.js",
+      //     "cartsApp": "http://localhost:4200/remoteEntry.js",
+      //     "ordersApp": "http://localhost:4000/remoteEntry.js",
+
+      // },
+
+      shared: share({
+        "@angular/core": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common/http": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/router": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+
+        ...sharedMappings.getDescriptors(),
+      }),
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
