7601848825f959c4c8e83a6f37a67b0a65ba2faf
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2023-12-14 00:04:18 -0300

    feat: fazendo request e criando variavel de ambiente no module federation

--- projects/products-app/src/app/products/products.component.ts
+++ projects/products-app/src/app/products/products.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  constructor(private productsService: ProductsService) {}
 
+  ngOnInit(): void {
+    this.productsService
+      .getAll()
+      .subscribe((products) => console.log(products));
+  }
 }
--- projects/products-app/webpack.config.js
+++ projects/products-app/webpack.config.js
@@ -3,17 +3,29 @@ const {
   withModuleFederationPlugin,
 } = require("@angular-architects/module-federation/webpack");
 
-module.exports = withModuleFederationPlugin({
-  name: "products",
-  // Aqui estou expondo para outros modulos o meu componente products
-  exposes: {
-    "./Module": "./projects/products-app/src/app/products/products.module.ts",
-  },
-  shared: {
-    ...shareAll({
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: "auto",
+const { EnvironmentPlugin } = require("webpack");
+
+module.exports = {
+  ...withModuleFederationPlugin({
+    name: "products",
+    // Aqui estou expondo para outros modulos o meu componente products
+    exposes: {
+      "./Module": "./projects/products-app/src/app/products/products.module.ts",
+    },
+    shared: {
+      ...shareAll({
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: "auto",
+      }),
+    },
+  }),
+  plugins: [
+    new EnvironmentPlugin({
+      BASE_URL: "https://fakestoreapi.com/products",
     }),
+  ],
+  devServer: {
+    webSocketServer: false,
   },
-});
+};
